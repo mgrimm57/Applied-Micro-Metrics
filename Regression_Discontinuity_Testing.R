32315820c66896252bdf32fec129f35d88b961ec
mgrimm57 <grimm78747@gmail.com>
2024-03-21 13:03:17 -0700

    last one

--- Regression_Discontinuity_Testing.R
+++ Regression_Discontinuity_Testing.R
@@ -8,14 +8,14 @@ pacman::p_load(tidyverse, haven, stargazer, rdd, ivreg, ggplot2, magrittr, ggpub
 p2 <- read_dta("DATA/RD_Manip_Econ587.dta")
 
 
-##### Part A #####
+# graph showing the impact of different bandwidths
 plot_0.1 <- ggplot(aes(reportwealth1), data = p2) + geom_density(bw=0.1, col = "green") + 
   geom_density(aes(reportwealth2), bw=0.1, col="blue") + 
   geom_density(aes(reportwealth3), bw=0.1, col="purple2") +
   geom_density(aes(reportwealth4), bw=0.1, col="red") + theme_linedraw() + 
   xlab("Bandwidth 0.1") + ylab("")
 
-plot_0.05 <- ggplot(aes(reportwealth1), data = p2) + geom_density(bw=0.1, col = "green") + 
+plot_0.05 <- ggplot(aes(reportwealth1), data = p2) + geom_density(bw=0.05, col = "green") + 
   geom_density(aes(reportwealth2), bw=0.05, col="blue") + 
   geom_density(aes(reportwealth3), bw=0.05, col="purple2") +
   geom_density(aes(reportwealth4), bw=0.05, col="red") + theme_linedraw() + 
@@ -30,8 +30,7 @@ plot_0.01 <- ggplot(aes(reportwealth1), data = p2) + geom_density(bw=0.01, col =
 ggarrange(plot_0.1, plot_0.05, plot_0.01, ncol=3)
 
 
-##### PART B #####
-## rough analysis for wealth1
+# crude test for smoothness around the cutoff for wealth1
 w1_1 = p2 |> filter(abs(reportwealth1) < 0.1) |> select(reportwealth1)|>
   ggplot(aes(reportwealth1)) + geom_histogram(bins = 15, fill = "lightgreen", color = "darkgreen") + 
   geom_vline(xintercept = 0, color = 'black', lwd = 2) +
@@ -49,7 +48,7 @@ w1_01 = p2 |> filter(abs(reportwealth1) < 0.01) |> select(reportwealth1)|>
 
 ggarrange(w1_1, w1_05, w1_01, ncol = 3)
 
-## rough analysis for wealth2
+# crude test for smoothness around the cutoff for wealth2
 w2_1 = p2 |> filter(abs(reportwealth2) < 0.1) |> select(reportwealth1)|>
   ggplot(aes(reportwealth1)) + geom_histogram(bins = 15, fill = "lightblue", color = "darkblue") + 
   geom_vline(xintercept = 0, color = 'black', lwd = 2) +
@@ -67,7 +66,7 @@ w2_01 = p2 |> filter(abs(reportwealth2) < 0.01) |> select(reportwealth1)|>
 
 ggarrange(w2_1, w2_05, w2_01, ncol = 3)
 
-## rough analysis for wealth3
+# crude test for smoothness around the cutoff for wealth3
 w3_1 = p2 |> filter(abs(reportwealth3) < 0.1) |> select(reportwealth1)|>
   ggplot(aes(reportwealth1)) + geom_histogram(bins = 15, fill = "lavender", color = "purple") + 
   geom_vline(xintercept = 0, color = 'black', lwd = 2) +
@@ -85,7 +84,7 @@ w3_01 = p2 |> filter(abs(reportwealth3) < 0.01) |> select(reportwealth1)|>
 
 ggarrange(w3_1, w3_05, w3_01, ncol = 3)
 
-## rough analysis for wealth4
+# crude test for smoothness around the cutoff for wealth4
 w4_1 = p2 |> filter(abs(reportwealth4) < 0.1) |> select(reportwealth1)|>
   ggplot(aes(reportwealth1)) + geom_histogram(bins = 15, fill = "pink", color = "red") + 
   geom_vline(xintercept = 0, color = 'black', lwd = 2) +
@@ -104,19 +103,19 @@ w4_01 = p2 |> filter(abs(reportwealth4) < 0.01) |> select(reportwealth1)|>
 ggarrange(w4_1, w4_05, w4_01, ncol = 3)
 
 
-##### PART C #####
+#McCrary and Kovak test
+
 vars = paste0("reportwealth", 1:4)
 sapply(vars, function(x){DCdensity(p2[[x]], cutpoint = 0)})
 
 
-##### PART D #####
-## create dummy variables
+# create dummy variables for welath less than 0
 p2 %<>% mutate(t1 = if_else(reportwealth1 < 0, 1, 0),
                t2 = if_else(reportwealth2 < 0, 1, 0),
                t3 = if_else(reportwealth3 < 0, 1, 0),
                t4 = if_else(reportwealth4 < 0, 1, 0))
 
-## making newwealth variable
+# altering the wealth using dummy and some noise
 p2 %<>% mutate(new_wealth1 = truewealth + 0.2 * t1 + rnorm(nrow(p2), 0, sqrt(0.01)),
                new_wealth2 = truewealth + 0.2 * t2 + rnorm(nrow(p2), 0, sqrt(0.01)),
                new_wealth3 = truewealth + 0.2 * t3 + rnorm(nrow(p2), 0, sqrt(0.01)),
@@ -135,7 +134,8 @@ summary_density <- ggplot(p2) + geom_density(aes(new_wealth1, colour= "New Wealt
 summary_density
 
 
-##### PART E #####
+# function to run multiple RD regressions
+
 normal_reg = lapply(1:4, function(i){ 
   # Generate formula
   y = paste0("new_wealth", i)
@@ -143,19 +143,17 @@ normal_reg = lapply(1:4, function(i){
   report = paste0("reportwealth", i)
   x = paste(treat, report, sep = "*")
   formula = paste(y, x, sep = "~")
-  
   # Run regression
   lm(formula, p2) |> summary()
 })
 
 
-##### PART F #####
+# a more robsut regression disontinuity design
 robust_reg = lapply(1:4, function(i){
   # Generate formula
   y = paste0("new_wealth", i)
   x = paste0("reportwealth", i)
   formula = paste(y, x, sep = "~")
-  
   # Run rdd 
   RDestimate(formula, p2)
 })
