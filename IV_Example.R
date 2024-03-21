f7987856d1617f326f4b934309e4a26ba5cb1b98
mgrimm57 <grimm78747@gmail.com>
2024-03-21 12:56:50 -0700

    last update for this

--- IV_Example.R
+++ IV_Example.R
@@ -1,52 +1,48 @@
 ### IV example using dinkelman 2011 paper
-### goal: find effect of electrifcation on proportion of women employed
+### goal: find effect of having electricity on proportion of women employed
 
 
 # load in data
 library(haven)
-df <- read_dta("Coursework/metrics/Year 2/587/CODE/DATA/dinkelman_aer2011_Econ587.dta")
-# set up controls
+df <- read_dta("DATA/dinkelman_aer2011_Econ587.dta")
+
+# set up controls that will be used throughout the analysis
 control = ~ kms_to_subs0+ baseline_hhdens0+ base_hhpovrate0+ prop_head_f_a0+
   sexratio0_a+ prop_indianwhite0+ kms_to_road0+ kms_to_town0+ prop_matric_m0+ 
   prop_matric_f0+ d_prop_waterclose+ d_prop_flush+ idcc1+idcc2+ idcc3+
   idcc4+ idcc5+ idcc6+ idcc7+ idcc8+ idcc9
 part_a = update(control, d_prop_emp_f ~ . + T)
 
-### part a
 
-# regression with clustered standard errors
+# regression with clustered standard errors as baseline
 library(estimatr)
 reg_a<- lm_robust(part_a, data = df, se_type = "stata", cluster = placecode0)
 summary(reg_a)
 
-### part b
-# T regression
+# Treatment regression
 part_b = update(control, T ~ . + mean_grad_new)
 reg_b<- lm_robust(part_b, data = df, se_type = "stata", cluster = placecode0)
 summary(reg_b)
 
-### part c
-
 # F-test robust to heteroskedasticity
 car::linearHypothesis(reg_b, "mean_grad_new=0")
 
-### part d
-
+# F-test done by hand just because why not
 F_by_hand = (reg_b[["statistic"]][["mean_grad_new"]])^2
 
-### part e
 
+# get predicitions for T based off of regression part_b
 get_T_hat = lm(part_b, data = df)
 df$T_hat = predict(get_T_hat)
+
+# using T hat instead of T now...
 part_e = update(control, d_prop_emp_f ~ . + T_hat)
 reg_e = lm_robust(part_e, data = df, se_type = "stata", cluster = placecode0)
 
-### part f
-
 summary(reg_e)
 
-### part g
-# estimate model
+
+# big long IV regression
 library(ivreg)
 ivmodel=ivreg(d_prop_emp_f ~  kms_to_subs0+ baseline_hhdens0+ base_hhpovrate0+ prop_head_f_a0+
                 sexratio0_a+ prop_indianwhite0+ kms_to_road0+ kms_to_town0+ prop_matric_m0+ 
@@ -58,25 +54,14 @@ ivmodel=ivreg(d_prop_emp_f ~  kms_to_subs0+ baseline_hhdens0+ base_hhpovrate0+ p
                 idcc5+ idcc6+ idcc7+ idcc8+ idcc9 + mean_grad_new, 
               data=df)
 
-### part h
-
+# reduced form regression for use in Wald test
 part_h = update(control, d_prop_emp_f ~ . + mean_grad_new)
 reg_h<- lm_robust(part_h, data = df, se_type = "stata", cluster = placecode0)
 summary(reg_h)
 
-### part i
 
+# Wald test 
 Wald = reg_h[["coefficients"]][["mean_grad_new"]] / reg_b[["coefficients"]][["mean_grad_new"]]
 
-### part j
 
-## i dont know how to do this in r :(
-
-### part k
-
-first_stage = lm(part_b, data=df)
-
-df$resid = first_stage[["residuals"]]
-part_k = update(control, d_prop_emp_f ~ . +T+ resid)
-second_stage = lm_robust(part_k,data=df, se_type="stata", cluster=placecode0)
 summary(second_stage)
